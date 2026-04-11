e5586bdc89b46079d44622224ecaed79d462b51e
chinmay <chinmaypujare07@gmail.com>
2026-04-11 23:55:01 +0530

    setup react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-//import Home from "./pages/Home"; // adjust path if needed
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home"; // adjust path if needed
 import About from "./pages/About";
+import { Routes, Route } from "react-router-dom";
 function App() {
-  return <About/> ;
-  
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} /> 
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />}/>   
+    </Routes>
+  ) ;
+   
   
 }
 
